b5e8d8edb9676a739debf2a25fbbabe48159bbae
4pydev <4pydev@gmail.com>
2018-02-27 23:07:00 +0300

    Relied get_courses_list()

--- coursera.py
+++ coursera.py
@@ -1,7 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
 
 
 def get_courses_list():
-    pass
+    courses_list_url = 'https://www.coursera.org/sitemap~www~courses.xml'
+    response_xml = requests.get(courses_list_url).content.decode()
+
+    xml_page = BeautifulSoup(response_xml, 'xml')
+
+    courses_urls = []
+    for num, loc in enumerate(xml_page.find_all('loc')):
+        if num < 20:
+            courses_urls.append(loc.text)
+    return courses_urls
 
 
 def get_course_info(course_slug):
@@ -13,4 +24,4 @@ def output_courses_info_to_xlsx(filepath):
 
 
 if __name__ == '__main__':
-    pass
+    print(get_courses_list())
