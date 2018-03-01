88ec888818d9eb94c1ecb50ca1327a964851897c
4pydev <4pydev@gmail.com>
2018-03-01 17:51:34 +0300

    Attempt #1 to realize clear funcs

--- coursera.py
+++ coursera.py
@@ -4,23 +4,32 @@ from bs4 import BeautifulSoup
 from openpyxl import Workbook
 
 
-def get_random_courses_url_list(number_of_courses):
+def get_courses_xml_page():
     courses_list_url = 'https://www.coursera.org/sitemap~www~courses.xml'
-    response_xml = requests.get(courses_list_url).content.decode()
+    xml_course_page = requests.get(courses_list_url).content.decode()
 
-    xml_page = BeautifulSoup(response_xml, 'xml')
+    return xml_course_page
+
+
+def get_random_courses_url_list(xml_course_page, number_of_courses):
+    xml_course_soup = BeautifulSoup(xml_course_page, 'xml')
 
     full_courses_urls = []
-    for loc in xml_page.find_all('loc'):
+    for loc in xml_course_soup.find_all('loc'):
         full_courses_urls.append(loc.text)
 
-    courses_urls = sample(full_courses_urls, number_of_courses)
+    random_courses_urls = sample(full_courses_urls, number_of_courses)
 
-    return courses_urls
+    return random_courses_urls
 
 
-def get_course_info(course_url):
+def get_html_course_page(course_url):
     course_page_html = requests.get(course_url).content.decode()
+    return course_page_html
+
+
+
+def get_course_info(course_page_html):
     course_soup = BeautifulSoup(course_page_html, 'lxml')
 
     course_name = course_soup.find(attrs={'class': 'course-title'}).text
@@ -79,10 +88,14 @@ if __name__ == '__main__':
     number_of_courses = 20
     path_to_output_xlsx = 'courses_info.xlsx'
 
-    courses_url_list = get_random_courses_url_list(number_of_courses)
+    xml_course_page = get_courses_xml_page()
+
+    courses_url_list = get_random_courses_url_list(xml_course_page,
+                                                   number_of_courses)
     courses_info_list = []
-    for course in courses_url_list:
-        courses_info_list.append(get_course_info(course))
+    for course_url in courses_url_list:
+        courses_info_list.append(get_course_info(
+            get_html_course_page(course_url)))
 
     current_workbook = output_courses_info_to_xlsx(courses_info_list)
     current_workbook.save(path_to_output_xlsx)
